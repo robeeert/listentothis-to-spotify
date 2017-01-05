99f4e9112118da4de369a0495d59d5ffaa3bfa60
Robert Pahl <pahl@mediaworx.com>
2017-01-05 14:11:12 +0100

    added main.js wich was forgotten in last commit

--- main.js
+++ main.js
@@ -1,51 +1,73 @@
-var https = require('https');
-
-var options = {
-  hostname: 'www.reddit.com',
-  path: '/r/listentothis/top.json?sort=top&t=week'
-};
+const https = require('https');
 
 function cleanRedditTitle(title) {
   return title.replace(/\[.*\]|\(.*\)/g, '');
 }
 
-https.get(options, function(res) {
-  var json = '';
+function getRedditWeeklyPosts(callback) {
 
-  res.on('data', function(chunk) {
-    json += chunk;
-  });
+  const options = {
+    hostname: 'www.reddit.com',
+    path: '/r/listentothis/top.json?sort=top&t=week&limit=25'
+  };
 
-  res.on('end', function() {
-    var list = JSON.parse(json);
+  https.get(options, function(res) {
+    let json = '';
 
-    var searchQuery = encodeURIComponent(cleanRedditTitle(list.data.children[2].data.title));
-
-    var spotifyOptions = {
-      hostname: 'api.spotify.com',
-      path: '/v1/search?query=' + searchQuery + '&offset=0&limit=20&type=track'
-    };
-
-    https.get(spotifyOptions, function(res) {
-      var json = '';
-
-      res.on('data', function(chunk) {
-        json += chunk;
-      });
-
-      res.on('end', function() {
-        var list = JSON.parse(json);
-
-        console.log(list);
-
-
-      });
-    }).on('error', function(e) {
-      console.log("Got error: " + e.message);
+    res.on('data', function(chunk) {
+      json += chunk;
     });
 
+    res.on('end', function() {
+      const list = JSON.parse(json);
 
+      let songs = [];
+
+      let length = 0;
+
+      list.data.children.forEach(function(post, index){
+
+        if (!post.is_self) {
+          searchSpotifyTrack(encodeURIComponent(cleanRedditTitle(post.data.title) ,function(searchResult){
+            length++;
+
+            if (searchResult.tracks.total && searchResult.tracks.total < 15) {
+              songs[index] = searchResult.tracks.items[0].id;
+            }
+
+            if (list.data.children.length === length) {
+              callback(songs);
+            }
+          })
+        }
+      });
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
   });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
+}
+
+function searchSpotifyTrack(query, callback) {
+  let spotifyOptions = {
+    hostname: 'api.spotify.com',
+    path: '/v1/search?query=' + query + '&offset=0&limit=20&type=track'
+  };
+
+  https.get(spotifyOptions, function(res) {
+    let json = '';
+
+    res.on('data', function(chunk) {
+      json += chunk;
+    });
+
+    res.on('end', function() {
+      callback(JSON.parse(json));
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+}
+
+getRedditWeeklyPosts(function(songs){
+  console.log(songs.filter(Boolean), songs.length);
 });
